62ef36621f57b406f07b946894aa0b7cf2596d8b
Isha Goyal <iamishagoyal@gmail.com>
2017-02-17 22:28:59 +0530

    add function,questions appearing in reverse stack order

--- QuestionAnswer.js
+++ QuestionAnswer.js
@@ -1,9 +1,30 @@
 var questionText;
+var questionArray=[];
+
 
 
 
 function addProblem(){
+	
+	var body=document.getElementsByTagName('body')[0];
 	questionText=document.getElementById('question').value;
-	console.log(questionText);
-	document.getElementById('questionText').innerHTML=questionText;
+	questionArray.unshift(questionText);
+
+    	
+	
+	var questionLogo = document.createElement("p");
+	var textNode = document.createTextNode("Question:");
+    var question=document.createElement("p");
+    var questionDetail=document.createTextNode(questionArray[0]);
+    var br=document.createElement("br");
+    
+      body.appendChild(questionLogo);
+	  questionLogo.appendChild(textNode);
+	  body.appendChild(question);
+	  question.appendChild(questionDetail);
+    
+    body.appendChild(br);
+
+		
+  
 }
