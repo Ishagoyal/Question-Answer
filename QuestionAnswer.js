74272085586120f116f00be56fb16769ee1f2f81
Isha Goyal <iamishagoyal@gmail.com>
2017-02-18 00:23:15 +0530

    ques ans appearing in stack order

--- QuestionAnswer.js
+++ QuestionAnswer.js
@@ -1,5 +1,8 @@
 var questionText;
+var answerText;
 var questionArray=[];
+var answerArray=[];
+var i=0;
 
 
 
@@ -8,22 +11,47 @@ function addProblem(){
 	
 	var body=document.getElementsByTagName('body')[0];
 	questionText=document.getElementById('question').value;
+    answerText=document.getElementById('answer').value;
 	questionArray.unshift(questionText);
+    answerArray.unshift(answerText);
 
-    	
-	
+    var myContainer=document.getElementById('container');
+    
+    var myDiv=document.createElement("div");
 	var questionLogo = document.createElement("p");
 	var textNode = document.createTextNode("Question:");
-    var question=document.createElement("p");
     var questionDetail=document.createTextNode(questionArray[0]);
-    var br=document.createElement("br");
-    
-      body.appendChild(questionLogo);
+
+
+    var answerLogo = document.createElement("p");
+    var ansTextNode = document.createTextNode("Answer: ");
+    var answerDetail=document.createTextNode(answerArray[0]);
+    var mybr=document.createElement("br");
+     if(i==0){
+      myContainer.appendChild(myDiv);
+      myDiv.appendChild(questionLogo);
 	  questionLogo.appendChild(textNode);
-	  body.appendChild(question);
-	  question.appendChild(questionDetail);
-    
-    body.appendChild(br);
+	  questionLogo.appendChild(questionDetail);
+      questionLogo.appendChild(mybr);
+      myDiv.appendChild(answerLogo);
+      questionLogo.appendChild(ansTextNode);
+      questionLogo.appendChild(answerDetail);
+      
+    }
+    else if(i>0){
+      myContainer.insertBefore(myDiv,myContainer.firstChild);
+      myDiv.appendChild(questionLogo);
+      questionLogo.appendChild(textNode);
+      questionLogo.appendChild(questionDetail);
+      questionLogo.appendChild(mybr);
+      myDiv.appendChild(answerLogo);
+      questionLogo.appendChild(ansTextNode);
+      questionLogo.appendChild(answerDetail);
+          
+    }
+
+    i++;
+   
 
 		
   
